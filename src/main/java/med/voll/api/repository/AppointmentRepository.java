896fbedecf0181fe137673605b89d98bcc69d905
jonhvtr <jonhvtr_dev@hotmail.com>
2025-08-05 15:21:58 -0300

    feat(appointment): implement scheduling feature with service, controller, and repository

---
+++ src/main/java/med/voll/api/repository/AppointmentRepository.java
@@ -0,0 +1,12 @@
+package med.voll.api.repository;
+
+import med.voll.api.domain.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    boolean existsByPacienteIdAndDataBetween(Long idPatient, LocalDateTime firstTime, LocalDateTime lastTime);
+
+    boolean existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(Long idDoctor, LocalDateTime dateTime);
+}
--- src/main/java/med/voll/api/repository/PatientRepository.java
+++ src/main/java/med/voll/api/repository/PatientRepository.java
@@ -4,7 +4,15 @@ import med.voll.api.domain.Patient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     Page<Patient> findByAtivoTrue(Pageable pageable);
+
+    @Query("""
+            select p.ativo
+            from Patient p
+            where p.id = :idPatient
+            """)
+    boolean findAtivoById(Long idPatient);
 }
