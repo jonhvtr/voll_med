1b5d6b218390ac032a8ed6cc39649a41b18ee2fe
jonhvtr <jonhvtr_dev@hotmail.com>
2025-09-01 15:42:31 -0300

    refactor(infra): add support UUID

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import med.voll.api.domain.entities.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.UUID;
 
 @Service
 public class TokenService {
@@ -19,15 +21,31 @@ public class TokenService {
     private String secret;
 
     public String generateToken(User user) {
+        UUID userId = UUID.randomUUID();
+
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create().withIssuer("API Voll.med")
                     .withSubject(user.getLogin())
-                    .withClaim("id", user.getId())
+                    .withClaim("id", user.getId().toString())
                     .withExpiresAt(dateExpires())
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar Token JWT: ", exception);
+        }
+    }
+
+    public UUID getUserId(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT decodedJWT = JWT.require(algorithm)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(token);
+
+            return UUID.fromString(decodedJWT.getClaim("id").asString());
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
